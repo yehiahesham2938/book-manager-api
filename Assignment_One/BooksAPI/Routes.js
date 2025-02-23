71dbc974bb76b1d82bb0b5db80328b7c4122e271
yehiahesham2938 <s-yehia.abdelrasol@zewailcity.edu.eg>
2025-02-24 01:01:20 +0200

    Completed the DELETE, Update(put) APIs and tested.

--- Assignment_One/BooksAPI/Routes.js
+++ Assignment_One/BooksAPI/Routes.js
@@ -20,7 +20,7 @@ Router.post('/books', async(request, response) => {
     const {
         title, author, published_year
     } = request.body;
-    const InsertBookQuery = 'INSERT INTO books (title, author, published_year) VALUES ($1, $2, $3) RETURNING *';
+    const InsertBookQuery = 'INSERT INTO books (title, author, published_year) values ($1, $2, $3) RETURNING *';
     try {
         const result = await pool.query(InsertBookQuery, [title, author, published_year]);
         response.status(201).json(result.rows[0]);
@@ -29,6 +29,24 @@ Router.post('/books', async(request, response) => {
     }
 
 });
+
+Router.put('/books/:id', async(request, response) => {
+    const {id} = request.params;
+    const {
+        title, author, published_year
+    } = request.body;
+    const updateBookQuery = "UPDATE books set title = $1, author = $2, published_year = $3 Where id = $4 RETURNING *";
+    const updateBook = await pool.query(updateBookQuery, [title,author,published_year,id]);
+    response.json(updateBook.rows[0]);
+})
+
+Router.delete('/books/:id', async(request,response ) => {
+    const{id} = request.params;
+    await pool.query('DELETE From books WHERE id = $1', [id]);
+    response.json({Book: 'Deleted Successfully...'});
+
+    
+})
 
 
 module.exports = Router;
